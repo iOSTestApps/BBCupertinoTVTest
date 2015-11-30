f569c9729c02c67d9c18c21fa850ffbb1e85afe3
Christopher Stott <christopherstott@gmail.com>
2015-11-29 19:39:19 -0800

    init

---
+++ BBCupertinoTVTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBCupertinoTVTest
+//
+//  Created by Chris on 2015-11-29.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBCupertinoTVTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBCupertinoTVTest
+//
+//  Created by Chris on 2015-11-29.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
